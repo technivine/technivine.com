50587351e1bfc4b497b1c46016aada783eb37040
firman herda <firmanherda26@gmail.com>
2024-12-04 08:55:15 +0700

    update

---
+++ src/app/header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="bg-blue-600 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-bold">My Website</h1>
+        <ul className="flex space-x-4">
+          <li><a href="#home" className="hover:underline">Home</a></li>
+          <li><a href="#about" className="hover:underline">About</a></li>
+          <li><a href="#contact" className="hover:underline">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
+import React from "react";
+import Header from "./header"; 
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       {/* Navbar */}
-      <nav className="bg-blue-600 text-white p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-xl font-bold">My Website</h1>
-          <ul className="flex space-x-4">
-            <li><a href="#home" className="hover:underline">Home</a></li>
-            <li><a href="#about" className="hover:underline">About</a></li>
-            <li><a href="#contact" className="hover:underline">Contact</a></li>
-          </ul>
-        </div>
-      </nav>
+      <Header />
 
       <div className="flex flex-grow">
         {/* Sidebar */}
