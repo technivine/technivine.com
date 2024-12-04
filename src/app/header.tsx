9324ffacbe5b3707d8bba15e526378ec2ac611bc
firman herda <firmanherda26@gmail.com>
2024-12-04 08:59:25 +0700

    update

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-xl font-bold">My Website</h1>
         <ul className="flex space-x-4">
-          <li><a href="#home" className="hover:underline">Home</a></li>
+          <li><a href="#home" className="hover:underline">Homee</a></li>
           <li><a href="#about" className="hover:underline">About</a></li>
           <li><a href="#contact" className="hover:underline">Contact</a></li>
         </ul>
